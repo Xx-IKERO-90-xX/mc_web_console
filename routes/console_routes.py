a4a021f9990fa9d72125a66c0298f6b2cba5e1a6
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2026-01-03 23:22:09 +0100

    Empezamos a crear la pagina de gestion de consolas remotas

--- routes/console_routes.py
+++ routes/console_routes.py
@@ -3,6 +3,7 @@ import sys
 from flask import request, Flask, render_template, redirect, session, sessions, url_for, Blueprint
 from extensions import db
 import json
+from models.Console import Console
 
 console_bp = Blueprint('console', __name__)
 
@@ -10,7 +11,9 @@ console_bp = Blueprint('console', __name__)
 @console_bp.route("/", methods=["GET"])
 async def index(): 
     if 'id' in session:
-        return render_template('console/index.jinja')
+        page = request.args.get('page', 1, type=int)
+        consoles = db.session.query(Console).paginate(page=page, per_page=5)
+        return render_template('console/index.jinja', consoles=consoles)
     
     return redirect(url_for('auth.login'))
 
